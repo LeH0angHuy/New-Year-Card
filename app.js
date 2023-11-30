2e96a7a1f06c02fa9199f5d36784b46d9e0c1130
LeH0angHuy <52832937+LeH0angHuy@users.noreply.github.com>
2023-11-30 20:16:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,86 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Particles.js configuration
-    var particlesConfig = {
-        particles: {
-            number: {
-                value: 150,
-                density: {
-                    enable: true,
-                    value_area: 800
-                }
-            },
-            color: {
-                value: "red" // Change the circle color to red
-            },
-            shape: {
-                type: ["image", "circle"],
-                image: {
-                    src: "star.png",
-                    width: 250,
-                    height: 250
-                }
-            },
-            opacity: {
-                value: 1,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 1,
-                    opacity_min: 0,
-                    sync: false
-                }
-            },
-            size: {
-                value: 3,
-                random: true,
-                anim: {
-                    enable: true,
-                    speed: 5,
-                    size_min: 0.1,
-                    sync: false
-                }
-            },
-            line_linked: {
-                enable: false
-            },
-            move: {
+// Particles.js configuration
+var particlesConfig = {
+    particles: {
+        number: {
+            value: 300,
+            density: {
                 enable: true,
-                speed: 3,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 1200
-                }
+                value_area: 800
             }
         },
-        interactivity: {
-            detect_on: "canvas",
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: "repulse"
-                },
-                onclick: {
-                    enable: true,
-                    mode: "push"
-                },
-                resize: true
+        color: {
+            value: "#ff0000" // Set the color to red
+        },
+        shape: {
+            type: ["image", "circle"],
+            image: {
+                src: "star.png",
+                width: 250,
+                height: 250
+            }
+        },
+        opacity: {
+            value: 1,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 5,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: false
+        },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
             },
-            modes: {
-                repulse: {
-                    distance: 100,
-                    duration: 0.4
-                },
-                push: {
-                    particles_nb: 4
-                }
-            }
+            onclick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
         },
-        retina_detect: true
-    };
+        modes: {
+            repulse: {
+                distance: 100,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            }
+        }
+    },
+    retina_detect: true
+};
 
-    particlesJS("particles-js", particlesConfig);
-});
+particlesJS("particles-js", particlesConfig);
